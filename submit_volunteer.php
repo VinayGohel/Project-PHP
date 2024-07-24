297573bc5a22da17f741ea67c501d6f2a1c29aec
VinayGohel <167532749+VinayGohel@users.noreply.github.com>
2024-07-24 19:04:45 +0530

    Update submit_volunteer.php

--- submit_volunteer.php
+++ submit_volunteer.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = htmlspecialchars($_POST['email']);
     $interests = htmlspecialchars($_POST['interests']);
     
-    // Here you can add code to save these details to a database or send an email
+   
 
     echo "Thank you for volunteering, $name!";
 } else {
