dce92d0242c442efaad076852c36a4bca6bffc41
VinayGohel <167532749+VinayGohel@users.noreply.github.com>
2024-07-24 19:02:41 +0530

    Update submit_donate.php

--- submit_donate.php
+++ submit_donate.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $donationAmount = htmlspecialchars($_POST['donation-amount']);
     $paymentMethod = htmlspecialchars($_POST['payment-method']);
     
-    // Here you can add code to process the donation
+    
 
     echo "Thank you for your generous donation of $donationAmount!";
 } else {
